684140a66d8760a2bed62fb00398a03ece0b9204
BaddiesScripted <likinggangfr3@gmail.com>
2026-04-19 03:40:23 +0000

    Create freemium.lua

---
+++ freemium.lua
@@ -0,0 +1,11 @@
+-- Simple Baddies Loader
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/bpremium.lua/refs/heads/main/bpremium.lua", true))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BaddiesScripted/bpremium.lua/refs/heads/main/ICON%20RICH", true))()
+end)
+
+print("Baddies loaded - check for GUI")
